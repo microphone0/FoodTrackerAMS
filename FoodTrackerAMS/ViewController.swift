7b18adbaf7045daca754144260869ee2d79100f4
Adam Saxton <ams17a@acu.edu>
2019-02-15 08:44:40 -0600

    Initial Commit

---
+++ FoodTrackerAMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerAMS
+//
+//  Created by Adam Saxton on 2/15/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
