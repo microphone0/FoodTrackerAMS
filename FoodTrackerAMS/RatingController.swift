74f8b0c59bd9217d31a78e7e5520ea870053c54f
Adam Saxton <ams17a@acu.edu>
2019-02-22 10:26:41 -0600

    Finished Working with view controllers, and part of implement a custom control.

---
+++ FoodTrackerAMS/RatingController.swift
@@ -0,0 +1,21 @@
+//
+//  RatingController.swift
+//  FoodTrackerAMS
+//
+//  Created by Adam Saxton on 2/22/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import UIKit
+
+class RatingController: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
