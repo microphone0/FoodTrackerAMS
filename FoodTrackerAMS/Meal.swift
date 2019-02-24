ddd94bfb83ed237acc9a9d3d4a5b1ac61b9e1ab4
Adam Saxton <ams17a@acu.edu>
2019-02-24 11:40:28 -0600

    Finished Define Your Data Model part of the tutorial.

---
+++ FoodTrackerAMS/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  FoodTrackerAMS
+//
+//  Created by Adam Saxton on 2/23/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
