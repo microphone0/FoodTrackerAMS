5f2d2a5c9b24affa31059aec36cb6c1505ffbb92
Adam Saxton <ams17a@acu.edu>
2019-02-25 06:10:08 -0600

    Started Create a Table View

---
+++ FoodTrackerAMS/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerAMS
+//
+//  Created by Adam Saxton on 2/25/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
